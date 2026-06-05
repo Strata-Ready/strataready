0b2c235d50d5b3014ed46f38f82bdee1d7791068
neurata <jeremy@neurata.ai>
2026-06-05 08:45:24 -0700

    fix: cache bust favicon with new filename

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,5 +1,5 @@
 export default function Logo({ size = 28 }: { size?: number }) {
   return (
-    <img src="/favicon.png" alt="StrataReady" style={{ width: size, height: size, borderRadius: size * 0.214 }} />
+    <img src="/favicon-new.png" alt="StrataReady" style={{ width: size, height: size, borderRadius: size * 0.214 }} />
   )
 }
